999b344f874abb28a4a33d3a97c291b6a1dd576c
CDFMLR <cdfmlr@163.com>
2023-04-29 12:43:07 +0800

    feat: add music player
    - add music player in BookReader
    - BookReader ControlPanel UI improvements for small screens (iPhone SE)

--- lib/bookreader.dart
+++ lib/bookreader.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:styled_widget/styled_widget.dart';
 
@@ -19,10 +20,14 @@ class _BookReaderState extends State<BookReader> {
   bool pagenated = false;
   final scaffoldState = GlobalKey<ScaffoldState>();
 
+  final player = AudioPlayer();
+
   @override
   void initState() {
     super.initState();
     _readBook();
+
+    player.play(UrlSource(exampleMusic[0]));
   }
 
   void _readBook() {
@@ -43,10 +48,6 @@ class _BookReaderState extends State<BookReader> {
   }
 
   void bookToPages() {
-    // split the book text into pages: By CharsPerPage()
-    // int charsPerPage = CharsPerPage(
-    //     textStyle, context.size?.width ?? 0, context.size?.height ?? 0);
-    // print("charsPerPage = $charsPerPage");
     pages.addAll(splitText(bookText));
   }
 
@@ -210,8 +211,8 @@ class _BookReaderState extends State<BookReader> {
           showModalBottomSheet(
               context: context,
               builder: (BuildContext context) {
-                return const Center(
-                  child: ControlPanel(),
+                return Center(
+                  child: ControlPanel(bookReader: this),
                 ).clipRRect(all: 16);
               });
         },
@@ -231,110 +232,147 @@ class _BookReaderState extends State<BookReader> {
 }
 
 class ControlPanel extends StatefulWidget {
-  const ControlPanel({Key? key}) : super(key: key);
+  final _BookReaderState bookReader;
+
+  const ControlPanel({Key? key, required this.bookReader}) : super(key: key);
 
   @override
-  _ControlPanelState createState() => _ControlPanelState();
+  _ControlPanelState createState() => _ControlPanelState(this.bookReader);
 }
 
 class _ControlPanelState extends State<ControlPanel> {
+  final _BookReaderState bookReader;
+
+  _ControlPanelState(this.bookReader);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Music Card
-        Row(children: [
-          Image.network("https://picsum.photos/200/200",
-                  width: 150, height: 150, fit: BoxFit.contain)
-              .clipRRect(topLeft: 16, bottomLeft: 16)
-              .padding(right: 8),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Hunch Gray", style: Theme.of(context).textTheme.labelLarge),
-              Text("ZUTOMAYO", style: Theme.of(context).textTheme.labelMedium),
-            ],
-          ),
-          const Spacer(),
+    // context.size.width => cannot get size during build ... you can use LayoutBuilder
+    return LayoutBuilder(builder: (context, constraints) {
+      Size size = constraints.biggest;
+      final musicImageWidth = min(size.width / 4, 150.0);
+      final buttonsHPadding = min(size.width / 16, 32.0);
+
+      return Column(
+        children: [
+          // Music Card
           Row(children: [
-            IconButton(
-                icon: const Icon(Icons.pause_rounded),
-                onPressed: () => {simpleAlert(context, "TODO", "pause")},
+            Image.network("https://picsum.photos/200/200",
+                    width: musicImageWidth,
+                    height: musicImageWidth,
+                    fit: BoxFit.contain)
+                .clipRRect(topLeft: 16, bottomLeft: 16)
+                .padding(right: 8),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Hunch Gray",
+                    style: Theme.of(context).textTheme.labelLarge),
+                Text("ZUTOMAYO",
+                    style: Theme.of(context).textTheme.labelMedium),
+              ],
+            ),
+            const Spacer(),
+            Row(children: [
+              IconButton(
+                  icon: const Icon(Icons.pause_rounded),
+                  onPressed: () => {
+                        // simpleAlert(context, "TODO", "pause"),
+                        bookReader.player.pause()
+                      },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context)
+                              .buttonTheme
+                              .colorScheme
+                              ?.onSecondary))),
+              const SizedBox(width: 8),
+              IconButton(
+                  icon: const Icon(Icons.skip_next_rounded),
+                  onPressed: () {
+                    // simpleAlert(context, "TODO", "next");
+                    final music =
+                        exampleMusic[Random().nextInt(exampleMusic.length)];
+                    bookReader.player.play(UrlSource(music));
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context)
+                              .buttonTheme
+                              .colorScheme
+                              ?.onSecondary))),
+            ]).padding(horizontal: 8),
+          ])
+              .card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)))
+              .padding(horizontal: 16, top: 16, bottom: 8),
+
+          // Search in the book
+          // InputChip(label: Text("Search in the book")),
+          TextField(
+            decoration: InputDecoration(
+                filled: true,
+                fillColor:
+                    Theme.of(context).buttonTheme.colorScheme?.onSecondary,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                suffixIcon: Icon(Icons.search),
+                labelText: 'Search in the book',
+                labelStyle: Theme.of(context).textTheme.labelMedium),
+          ).padding(horizontal: 16, vertical: 8),
+
+          // Reader Theme
+          TextButton(
+              child: Row(children: [
+                Text("Font & Theme",
+                    style: Theme.of(context).textTheme.labelMedium),
+                Spacer(),
+                Icon(Icons.text_fields),
+              ]).padding(all: 8),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Theme.of(context).buttonTheme.colorScheme?.onSecondary)),
+              // 太优雅了吧！设置个破颜色，简直就是工业奇迹！
+              onPressed: () => {
+                    simpleAlert(context, "TODO", "Theme")
+                  }).padding(horizontal: 16, vertical: 8),
+
+          // Buttons
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            TextButton(
+                onPressed: () => {simpleAlert(context, "TODO", "share")},
+                child: Icon(Icons.share)
+                    .padding(horizontal: buttonsHPadding, vertical: 8),
                 style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all(Theme.of(context)
                         .buttonTheme
                         .colorScheme
                         ?.onSecondary))),
-            const SizedBox(width: 8),
-            IconButton(
-                icon: const Icon(Icons.skip_next_rounded),
-                onPressed: () => {simpleAlert(context, "TODO", "next")},
+            TextButton(
+                onPressed: () => {simpleAlert(context, "TODO", "info")},
+                child: Icon(Icons.info)
+                    .padding(horizontal: buttonsHPadding, vertical: 8),
                 style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all(Theme.of(context)
                         .buttonTheme
                         .colorScheme
                         ?.onSecondary))),
-          ]).padding(horizontal: 8),
-        ])
-            .card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)))
-            .padding(horizontal: 16, top: 16, bottom: 8),
-
-        // Search in the book
-        // InputChip(label: Text("Search in the book")),
-        TextField(
-          decoration: InputDecoration(
-              filled: true,
-              fillColor: Theme.of(context).buttonTheme.colorScheme?.onSecondary,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(32),
-              ),
-              suffixIcon: Icon(Icons.search),
-              labelText: 'Search in the book',
-              labelStyle: Theme.of(context).textTheme.labelMedium),
-        ).padding(horizontal: 16, vertical: 8),
-
-        // Reader Theme
-        TextButton(
-            child: Row(children: [
-              Text("Font & Theme",
-                  style: Theme.of(context).textTheme.labelMedium),
-              Spacer(),
-              Icon(Icons.text_fields),
-            ]).padding(all: 8),
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                    Theme.of(context).buttonTheme.colorScheme?.onSecondary)),
-            // 太优雅了吧！设置个破颜色，简直就是工业奇迹！
-            onPressed: () => {
-                  simpleAlert(context, "TODO", "Theme")
-                }).padding(horizontal: 16, vertical: 8),
-
-        // Buttons
-        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          TextButton(
-              onPressed: () => {simpleAlert(context, "TODO", "share")},
-              child: Icon(Icons.share).padding(horizontal: 32, vertical: 8),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Theme.of(context).buttonTheme.colorScheme?.onSecondary))),
-          TextButton(
-              onPressed: () => {simpleAlert(context, "TODO", "info")},
-              child: Icon(Icons.info).padding(horizontal: 32, vertical: 8),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Theme.of(context).buttonTheme.colorScheme?.onSecondary))),
-          TextButton(
-              onPressed: () => {simpleAlert(context, "TODO", "bookmark")},
-              child: Icon(Icons.bookmark).padding(horizontal: 32, vertical: 8),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Theme.of(context).buttonTheme.colorScheme?.onSecondary)))
-        ]).padding(horizontal: 16, top: 8, bottom: 16)
-      ],
-    ).padding(all: 16);
+            TextButton(
+                onPressed: () => {simpleAlert(context, "TODO", "bookmark")},
+                child: Icon(Icons.bookmark)
+                    .padding(horizontal: buttonsHPadding, vertical: 8),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Theme.of(context)
+                        .buttonTheme
+                        .colorScheme
+                        ?.onSecondary)))
+          ]).padding(horizontal: 16, top: 8, bottom: 16)
+        ],
+      ).padding(all: 16);
+    });
   }
 }
 
@@ -346,3 +384,8 @@ void simpleAlert(BuildContext context, String title, String? content) {
     },
   );
 }
+
+const exampleMusic = [
+  "https://upload.wikimedia.org/wikipedia/commons/8/83/Auferstehung_Hannover_Orgel_BWV_532.ogg",
+  "https://upload.wikimedia.org/wikipedia/commons/9/97/Bach%2C_Johann_Sebastian_-_Suite_No.2_in_B_Minor_-_X._Badinerie.ogg"
+];
