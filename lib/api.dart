35a73f7d6fa2ad4e519ae4642f8852dea395d504
CDFMLR <cdfmlr@163.com>
2023-05-16 20:17:54 +0800

    feat: support AudioFileStore proxy
    murecom 得到没有 host 的 music.sourceUrl 时，
    前面加上 murecomServer 的 host。
    
    支持 murecom-gw4reader 9145ce2 的代理功能：
    
    - https://github.com/murchinroom/murecom-gw4reader/commit/9145ce24228bf6652512eab818f22a1c686b0ba5

--- lib/api.dart
+++ lib/api.dart
@@ -3,21 +3,31 @@ import 'package:http/http.dart' as http;
 
 import 'package:mureaderui/model.dart';
 
-const String murecomServer = "https://mureader.murchinroom.fun/murecom4reader/";
+/// murecomServer is the base url to murecom-gw4reader service
+const String murecomServer = "https://mureader.murchinroom.fun/murecom4reader";
 
 Uri murecomUri() {
-  return Uri.parse("${murecomServer}murecom");
+  return Uri.parse("$murecomServer/murecom");
 }
 
 Future<MurecomResponse> murecom(MurecomRequest request) async {
-  final response = await http.post(murecomUri(), body: jsonEncode(request.toJson()));
+  final response =
+      await http.post(murecomUri(), body: jsonEncode(request.toJson()));
 
-  if (response.statusCode == 200) {
-    final json = jsonDecode(response.body);
-    print("murecom response: $json");
-    return MurecomResponse.fromJson(json);
-  } else {
+  if (response.statusCode != 200) {
     print("murecom failed: ${response.statusCode}: ${response.body}");
     throw Exception("murecom failed: ${response.statusCode}: ${response.body}");
   }
+
+  final json = jsonDecode(response.body);
+  print("murecom response: $json");
+
+  var resp = MurecomResponse.fromJson(json);
+
+  // relative url: 视为 proxied: 前面加上 murecom-gw4reader 服务的地址
+  if (resp.music?.sourceUrl?.startsWith("/") ?? false) {
+    resp.music?.sourceUrl = "$murecomServer${resp.music?.sourceUrl}";
+  }
+
+  return resp;
 }
